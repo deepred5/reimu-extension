b381a13f1d9521e6b059f87f91edf9258fe1fe80
deepred5 <948931744@qq.com>
2016-03-24 12:47:26 +0800

    Update reimu.js

--- js/reimu.js
+++ js/reimu.js
@@ -1,6 +1,7 @@
 chrome.storage.local.set({'finalPassword': '8酱'});
 
 var pre = document.getElementsByTagName('pre')[0];
+
 if (pre && window.getComputedStyle(pre, null).display === 'none') {
 	pre.style.display = 'block';
 }
