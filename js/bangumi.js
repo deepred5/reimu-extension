e094d3f50f173342049450398cbc6611a83ee1c0
深红 <948931744@qq.com>
2016-04-16 22:33:23 +0800

    update

--- js/bangumi.js
+++ js/bangumi.js
@@ -1,36 +1,35 @@
-(function() {
-	var currentPosition = 0;
-	var timer = null;
 
-	function getTop(index) {
-		var day = document.querySelector('[weekday="' + index + '"]');
-		var distance = day.offsetTop;
+var currentPosition = 0;
+var timer = null;
+
+function getTop(index) {
+	var day = document.querySelector('[weekday="' + index + '"]');
+	var distance = day.offsetTop;
+	day = day.offsetParent;
+
+	while(day) {
+		distance += day.offsetTop;
 		day = day.offsetParent;
-
-		while(day) {
-			distance += day.offsetTop;
-			day = day.offsetParent;
-		}
-
-		return distance;
 	}
 
-	function runToTop(distance) {
-		currentPosition = document.body.scrollTop;
-		currentPosition += 15;
+	return distance;
+}
 
-		if (currentPosition >= distance) {
-			currentPosition = distance;
-			clearInterval(timer);
-		} 
+function runToTop(distance) {
+	currentPosition = document.body.scrollTop;
+	currentPosition += 15;
 
-		document.body.scrollTop = currentPosition;
+	if (currentPosition >= distance) {
+		currentPosition = distance;
+		clearInterval(timer);
+	} 
 
-	}
+	document.body.scrollTop = currentPosition;
 
-	if (document.querySelector('[weekday]')) {
-		var timer = setInterval(function() {
-			runToTop(getTop(new Date().getDay()));
-		}, 1);
-	}
-})()
+}
+
+if (document.querySelector('[weekday]')) {
+	timer = setInterval(function() {
+		runToTop(getTop(new Date().getDay()));
+	}, 1);
+}
