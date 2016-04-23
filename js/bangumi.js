d67a736eafc767284046048ab29a7d4395d9be4a
深红 <948931744@qq.com>
2016-04-23 13:15:38 +0800

    update

--- js/bangumi.js
+++ js/bangumi.js
@@ -1,42 +1,44 @@
-var currentPosition = 0;
-var oldPosition = 0;
-var timer = null;
+(function() {
+	var currentPosition = 0;
+	var oldPosition = 0;
+	var timer = null;
 
-function getTop(index) {
-	var day = document.querySelector('[weekday="' + index + '"]');
-	var distance = day.offsetTop;
-	day = day.offsetParent;
-
-	while(day) {
-		distance += day.offsetTop;
+	function getTop(index) {
+		var day = document.querySelector('[weekday="' + index + '"]');
+		var distance = day.offsetTop;
 		day = day.offsetParent;
+
+		while(day) {
+			distance += day.offsetTop;
+			day = day.offsetParent;
+		}
+
+		return distance;
 	}
 
-	return distance;
-}
+	function runToTop(distance) {
 
-function runToTop(distance) {
+		oldPosition = currentPosition;
+		currentPosition = document.body.scrollTop;
+		currentPosition += 15;
 
-	oldPosition = currentPosition;
-	currentPosition = document.body.scrollTop;
-	currentPosition += 15;
+		//滚动条滚动到最低端
+		if (oldPosition === currentPosition) {
+			clearInterval(timer);
+		}
 
-	//滚动条滚动到最低端
-	if (oldPosition === currentPosition) {
-		clearInterval(timer);
+		if (currentPosition >= distance) {
+			currentPosition = distance;
+			clearInterval(timer);
+		} 
+
+		document.body.scrollTop = currentPosition;
+
 	}
 
-	if (currentPosition >= distance) {
-		currentPosition = distance;
-		clearInterval(timer);
-	} 
-
-	document.body.scrollTop = currentPosition;
-
-}
-
-if (document.querySelector('[weekday]')) {
-	timer = setInterval(function() {
-		runToTop(getTop(new Date().getDay()));
-	}, 1);
-}
+	if (document.querySelector('[weekday]')) {
+		timer = setInterval(function() {
+			runToTop(getTop(new Date().getDay()));
+		}, 1);
+	}
+})();
